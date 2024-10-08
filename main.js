340aa7c46dcaf3bf664d79ce4739e78dd6ca3906
John Beltrez <beltrezjohn@gmail.com>
2024-10-08 16:08:50 -0400

    Added two more event listeners to button

--- main.js
+++ main.js
@@ -1,45 +1,37 @@
 // alert('Create your virtual Birthday Card!')
+    // let name = document.getElementById("name");
 function generateCard(event) {
-
-    event.preventDefault();
+    
+    // event.preventDefault();
     // const form = document.querySelector("#create-card");
     let name = document.getElementById("name");
+    let head = document.querySelector("h1");
+    console.log(head)
+    head.textContent += `\n ${name.value}`;
+
+    let message = document.getElementById("msg");
+    console.log(message)
+    let inputMessage = document.createElement("p");
+    console.log(inputMessage)
+    inputMessage.innerHTML = message.value
+    let cardBody = document.querySelector(".card-body");
+    cardBody.appendChild(inputMessage);
+    
+    
+
     let cardname = name.value;
     let h1 = document.getElementById("head");
-    h1.innerHTML += cardname.innerHTML;
-
-    
-
-    
+    let cardHead = document.querySelector(".card-header");
+    // let godOfWar = document.createElement("p");
+    // godOfWar.textContent = "hahahaha"
+    // cardHead.appendChild(godOfWar);
 }
-
 const customizeBtn = document.getElementById("button");
-customizeBtn.addEventListener("click", generateCard(e));
-// const card = document.createElement('div');
-// const cardHeader = document.createElement('div');
-// const message = document.createElement('p')
 
-// <div class="card">
-//             <div class="card-header">
-//                 <h1>Happy Birthday!</h1>
-//             </div>
-//             <div class="card-body">
-//                 <p id="message">Wishing you all the best on your special day!</p>
-//                 <ul class="gifts">
-//                     <li id="gift1">Gift card</li>
-//                     <li id="gift2">Dinner</li>
-//                     <li id="gift3">100 bucks</li>
-//                     <li id="gift-adder">Add Gift</li>
-//                 </ul>
-//                 /* <p>Do you like your gifts?</p>
-//                 <input type="radio" id="yes" name="liked-gifts" value="yes">
-//                 <label for="yes">Yes</label><br>
-//                 <input type="radio" id="no" name="liked-gifts" value="no">
-//                 <label for="no">No</label><br> */
-
-//             </div>
-//             <div class="card-footer">
-//                 <p id="customize-btn">Customize Message</p>
-//             </div>
-//         </div>
-    
+customizeBtn.addEventListener("click", generateCard);
+customizeBtn.addEventListener("mouseover", (e) => {
+    customizeBtn.style.backgroundColor = "blue";
+})
+customizeBtn.addEventListener("mouseout", (e) => {
+    customizeBtn.style.backgroundColor = "green";
+})
