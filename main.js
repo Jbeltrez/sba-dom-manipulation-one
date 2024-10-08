63485532f4bc24c6926a71dd215a5263c278f503
John Beltrez <beltrezjohn@gmail.com>
2024-10-08 14:29:41 -0400

    redid html and js file

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-window.onload = function() {
-    
-    // const userMessage = prompt('Enter a Birthday Message')
-if (userMessage) {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = userMessage;
+function generateCard(event) {
+
+    // event.preventDefault();
+    const form = document.querySelector("#create-card");
+    form.remove();
+
+
 }
-};
 
-const addGift = document.querySelector("#gift-adder");
-const giftList = document.querySelector('.gifts');
+const customizeBtn = document.getElementById('button');
+customizeBtn.addEventListener('click', generateCard);
+// const card = document.createElement('div');
+// const cardHeader = document.createElement('div');
+// const message = document.createElement('p')
 
-addGift.addEventListener('click', (e) => {
-    newGift = document.createElement("li");
-    newGift.innerHTML = "new gift";
+// <div class="card">
+//             <div class="card-header">
+//                 <h1>Happy Birthday!</h1>
+//             </div>
+//             <div class="card-body">
+//                 <p id="message">Wishing you all the best on your special day!</p>
+//                 <ul class="gifts">
+//                     <li id="gift1">Gift card</li>
+//                     <li id="gift2">Dinner</li>
+//                     <li id="gift3">100 bucks</li>
+//                     <li id="gift-adder">Add Gift</li>
+//                 </ul>
+//                 /* <p>Do you like your gifts?</p>
+//                 <input type="radio" id="yes" name="liked-gifts" value="yes">
+//                 <label for="yes">Yes</label><br>
+//                 <input type="radio" id="no" name="liked-gifts" value="no">
+//                 <label for="no">No</label><br> */
 
+//             </div>
+//             <div class="card-footer">
+//                 <p id="customize-btn">Customize Message</p>
+//             </div>
+//         </div>
     
-})
